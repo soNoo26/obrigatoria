99ee1612ad1e4d157b5b20f4ca4e4948f05da05c
Chico <132677444+soNoo26@users.noreply.github.com>
2024-05-23 13:27:41 -0300

    Update index.tsx

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,80 +1,75 @@
 import { useState } from "react";
-import { View, Text, Pressable, TextInput, StyleSheet, Alert, Image } from "react-native";
-
-export default function Login(){
-    const [email, setEmail] = useState('')
-    const [senha, setSenha] = useState('')
+import { View, Text, TextInput, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Icon from 'react-native-vector-icons/Feather';
+import { Link } from "expo-router";
 
 
-    function validarLogin(email: string, senha: string)
-    {
-        if(email == null || email == "")
-        {
-            
-            Alert.alert(
-                "Ops...",
-                "O email não pode ser vazio ... :(",
-                [{
-                    text: 'Ok'
-                }]
-            )
+export default function Home() {
+    const [passwordVisible, setPasswordVisible] = useState(false);
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+
+    const handleEmailChange = (text) => {
+        setEmail(text);
+        if (text.includes('@')) {
+            setEmailError('');
+        } else {
+            setEmailError('Error message');
         }
-        if( senha == null || senha == "" || senha.length < 10)
-        {
-            Alert.alert(
-                "Ops...",
-                "Senha invalida",
-                [{
-                    text: 'Ok'
-                }]
-            )
-        }
-    }
+    };
 
     return(
         <View style={estilo.tela}>
             <View style={estilo.container}>
-        <Image
-        style={estilo.logo}
-        source={{
-          uri: 'https://upload.wikimedia.org/wikipedia/commons/8/8c/SENAI_S%C3%A3o_Paulo_logo.png',
-        }}
-      />
-                <TextInput 
-                    style={estilo.text_input} 
-                    placeholder="Email..."
-                    onChangeText={text => setEmail(text)}
-                /> 
-
-                <TextInput 
-                    secureTextEntry={true} 
-                    style={estilo.text_input} maxLength={10}
-                    placeholder="Senha..."
-                    onChangeText={text => setSenha(text)}
+            <Image style={estilo.logo}
+                        source={{
+                            uri: 'https://i0.wp.com/smilephoto.com.br/wp-content/uploads/2018/05/sesi-logo.png?ssl=1'
+                        }}
+                    />
+                </View>
+                <TextInput
+                    placeholder="Email"
+                    style={[estilo.inputemail, emailError ? estilo.Errorinp : null]}
+                    keyboardType="email-address"
+                    onChangeText={handleEmailChange}
+                    value={email}
                 />
-                <Pressable 
-                    style={estilo.botao_entrar}
-                    onPress={()=> validarLogin(email, senha)}
+                {emailError ? <Text style={estilo.errorText}>{emailError}</Text> : null}
+                <View style={estilo.inputContainer}>
+                <View style={estilo.inputContainer}>
+                <TextInput
+                placeholder="Senha"
+                style={estilo.input} maxLength={10}
+                secureTextEntry={!passwordVisible}
+                />
+                <TouchableOpacity
+                onPress={() => setPasswordVisible(!passwordVisible)}
+                style={estilo.eyeIcon}
                 >
-                    <Text>Entrar</Text>
-                </Pressable>
+                    <Icon name = {passwordVisible ? "eye" : "eye-off"} size={24} color="gray" />
+                </TouchableOpacity>
+                </View>
+                <Link href={"#"} style={estilo.btnnew}>Cadastrar</Link>
+                <Link href={"#"} style={estilo.btnentrar}>Entrar</Link>
             </View>
         </View>
-    )
+    );
 }
+
 
 const estilo = StyleSheet.create({
     tela: {
-        flex: 1, 
-        padding: 10, 
-        alignItems: "center", 
-        justifyContent: "center"
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
     },
+
     container: {
+        alignItems: 'center',
         gap: 15,
         borderRadius: 20,
-        padding: 30,
-        backgroundColor: '#DE3C4B',
+        padding: 15,
+        backgroundColor: '#fff',
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -84,26 +79,63 @@ const estilo = StyleSheet.create({
         shadowRadius: 6.68,
         elevation: 11,
     },
-    text_input: {
-        height: 70,
-        width: 280,
-        backgroundColor: '#FFF',
-        fontSize: 18,
-        //fontWeight: 'bold',
-        padding: 10,
-        borderRadius: 20,
-    },
-    botao_entrar: {
-        padding: 15,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 20,
-        backgroundColor: '#fff'
-    },
-    logo: {
-        marginLeft: 8,
-        height: 70,
-        width:270,
-    }
 
+
+    input: {
+        marginBottom: 15,
+        padding: 1,
+        fontSize: 20,
+        width: 280,
+    },
+
+    Errorinp: {
+        borderBottomColor: 'red',
+    },
+
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingRight: 5,
+        width: 280,
+    },
+
+    inputemail: {
+        marginTop: 20,
+        borderBottomWidth: 1,    
+        padding: 1,
+        fontSize: 20,
+        width: 280,
+    },
+
+    eyeIcon: {
+        position: 'absolute',
+        right: 10,
+    },
+
+    logo: {
+        height: 80,
+        width: 270
+    },
+
+    btnentrar: {
+        textAlign: 'center',
+        backgroundColor: 'red',
+        borderRadius: 15,
+        width: 250,
+        fontSize: 23,
+        color: 'white',
+        padding: 5
+    },
+
+    btnnew: {
+        fontSize: 23
+    },
+
+    errorText: {
+        color: 'red',
+        fontSize: 14,
+        marginBottom: 15,
+    }
 })
